a2f667319b658c2421727ac1f150bbb0faa3bc45
uhablog <uha.blog98@gmail.com>
2023-02-14 21:34:42 +0900

    バリデーションチェック追加

--- src/main/java/com/uhablog/controller/BookController.java
+++ src/main/java/com/uhablog/controller/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,7 +52,13 @@ public class BookController {
      * @return
      */
     @PostMapping("/book-create")
-    public String saveBook(@ModelAttribute BookForm bookForm, Model model) {
+    public String saveBook(@ModelAttribute @Validated BookForm bookForm, BindingResult result,Model model) {
+    	
+    	// バリデーションエラーの場合
+    	if (result.hasErrors()) {
+    		// 新規登録画面に遷移
+    		return "add";
+    	}
 
         // 本を登録する
         service.insert(bookForm);
--- src/main/java/com/uhablog/form/BookForm.java
+++ src/main/java/com/uhablog/form/BookForm.java
@@ -1,17 +1,25 @@
 package com.uhablog.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class BookForm {
     /**
      * 本のタイトルを保持する
+     * チェック内容：空でないこと
      */
+    @NotBlank(message="タイトルを入力してください。")
     private String title;
 	
     /**
      * 本の値段を保持する
+     * チェック内容：Nullでないこと、プラスであること
      */
+    @NotNull(message="値段を入力してください。")
+    @Positive(message="値段はプラスの値を入力してください。")
     private Integer price;
 
 }
